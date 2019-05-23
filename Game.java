900989856f510491082fdeea432bf5a1e321f3e5
Gaetano Carfì <49553160+tanocarfi@users.noreply.github.com>
2019-05-23 17:40:59 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -9,11 +9,11 @@ public class Game {
     private int[][] grid;
     private int[] redNumbers = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
     private int[] blackNumbers = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
-    private enum Color {
+    /*private enum Color {
         GREEN,
         BLACK,
         RED;
-    }
+    }*/
 
     public Game() {
         this.grid = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 }, { 13, 14, 15 }, { 16, 17, 18 },
